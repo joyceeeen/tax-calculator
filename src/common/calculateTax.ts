65ea3fece92688544adaec821e280bd9d1c22759
Joyce <joycenpotestades@gmail.com>
2025-12-10 03:24:19 +0800

    add more tests and adjust calc

---
+++ src/common/calculateTax.ts
@@ -0,0 +1,26 @@
+import { strict as assert } from "node:assert";
+import type { CalculateTaxParams } from "../types/types";
+import { taxTable } from "./taxTable";
+
+export const calculateTax = (params: CalculateTaxParams): number => {
+  const { financialYear, income } = params;
+  const brackets = taxTable[financialYear];
+
+  const bracketIndex = brackets.findIndex(
+    (bracket) => income >= bracket.min && income <= bracket.max,
+  );
+
+  if (bracketIndex === -1) return 0;
+
+  const currentBracket = brackets[bracketIndex];
+
+  assert(
+    currentBracket,
+    "Current bracket not found in the tax table, this should never happen!",
+  );
+
+  const { baseAmount, rate } = currentBracket;
+  const previousBracketMax = brackets[bracketIndex - 1]?.max ?? 0;
+
+  return baseAmount + (income - previousBracketMax) * rate;
+};
